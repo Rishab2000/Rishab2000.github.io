f7dc433568f70ce293290e444e5e05b0781ecbf9
Rishab2000 <rishabsachidanand@gmail.com>
2020-09-25 16:01:51 +0530

    Complete About Page & Overall Optimizations

---
+++ About/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+ $(".nav-icon3").click(function () {
+  $(this).toggleClass("open");
+ });
+});
+
+function menuSlide() {
+ $(".mobile_nav").toggleClass("show");
+ $(".nav-icon3-bar").toggleClass("nav-icon3-black");
+}
+
+$(".nav-icon3").on("click", function () {
+ menuSlide();
+});
